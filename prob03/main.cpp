9974632a5ebf9dd3e15769213e5596df796f66fe
ardo1488 <ardo1488@csu.fullerton.edu>
2019-09-14 14:06:57 -0700

    debugged the program of its erros and spacing style

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,29 @@
 // Sales prediction
+
+#include <iostream>
+
+int main()
+{
+ // Last year's sales
+ double last_year = 2103419277;
+
+ // Display last year's Sales
+
+ std::cout << "Last year's sales were $"<< last_year <<"\n";
+
+ // the constant for the increase in sales this year
+
+ const double INCREASE = .18;
+
+ // calculate this years projected sales and make a variable for it
+
+ double this_year = (last_year * INCREASE) + last_year;
+
+ // dislay next this years projected Sales
+
+ std::cout << "This year's sales prediction: $"<< this_year<<"\n";
+
+ return 0;
+
+
+}
